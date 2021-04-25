45ec0977f7938e758c863d8ac83bf5f06b520998
Amamdi <amamdi25@yahoo.com>
2021-04-25 14:23:07 +0100

    Update

--- venv/amamdi/complainer/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from .views import SignUpView
-from django.urls import path, include
-from django.contrib.auth import views as auth_views
-from accounts import views
-
-urlpatterns = [
-    path('signup/', SignUpView.as_view(), name='signup'),
-    path('social-auth/', include('social_django.urls', namespace="social")),
-]
--- venv/amamdi/complainer/naijacomplainer/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms  
-from .models import Complainer  
-
-
-class ComplainerForm(forms.ModelForm):  
-    class Meta:  
-        model = Complainer  
-        fields = "__all__"
