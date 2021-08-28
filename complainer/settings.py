d08444edc41a105eca30d3227ce49a29c5acace6
Amamdi <amamdi25@yahoo.com>
2021-08-28 07:19:21 +0100

    update

--- complainer/settings.py
+++ complainer/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
+import dotenv
 import dj_database_url
 from pathlib import Path
 
@@ -21,8 +22,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
+dotenv_file = os.path.join(BASE_DIR, ".env")
+if os.path.isfile(dotenv_file):
+    dotenv.load_dotenv(dotenv_file)
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'rx7@+j)mmntzg!h(i$4#3c(d3mo_=&!j#%rlj99wg7z$^5r5ju'
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
